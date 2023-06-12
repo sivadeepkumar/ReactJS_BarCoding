814fbe6c4ca8fd561e344528c055ea27735ca61b
sivadeepkumar <sivadeepkumar3@gmail.com>
2023-06-12 11:59:57 +0000

    ReactJS_BarChart_Development

---
+++ src/components/VideoPlayer/index.js
@@ -0,0 +1,59 @@
+import './index.css'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+
+const data = [
+  {
+    group_name: 'Group A',
+    boys: 200,
+    girls: 400,
+  },
+  {
+    group_name: 'Group B',
+    boys: 3000,
+    girls: 500,
+  },
+  {
+    group_name: 'Group C',
+    boys: 1000,
+    girls: 1500,
+  },
+  {
+    group_name: 'Group D',
+    boys: 700,
+    girls: 1200,
+  },
+]
+
+const VideoPlayer = () => {
+  const DataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart data={data} margin={{top: 5}}>
+        <XAxis dataKey="group_name" tick={{stroke: 'red', strokeWidth: 1}} />
+
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{stroke: 'red', strokeWidth: 0}}
+        />
+
+        <Legend wrapperStyle={{padding: 0}} />
+        <Bar dataKey="boys" name="Boys" fill="#1f77b4" barSize="20%" />
+        <Bar dataKey="girls" name="Girls" fill="#fd7f0e" barSize="20%" />
+      </BarChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default VideoPlayer
